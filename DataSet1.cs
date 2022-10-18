5dc16084166f73f7921c3d23b1457fbe003f6746
SarvarGulyaev <89157908+SarvarGulyaev@users.noreply.github.com>
2022-10-18 23:23:16 +0300

    'v1'

---
+++ DataSet1.cs
@@ -0,0 +1,23 @@
+﻿namespace POSales
+{
+
+
+    partial class DataSet1
+    {
+        partial class dtStockInHistDataTable
+        {
+        }
+
+        partial class dtCancelOrderDataTable
+        {
+        }
+
+        partial class dtInventoryDataTable
+        {
+        }
+
+        partial class dtReceptDataTable
+        {
+        }
+    }
+}
